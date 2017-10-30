45fcf94355df846a2a67b384e059020cc17f0069
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2017-10-29 20:25:59 -0500

    Update taller1.c

--- taller1.c
+++ taller1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXLINE 1000 /* maximum input line length */
 
@@ -12,21 +13,60 @@ main()
 {
     int len; /* current line length */
     int max; /* maximum length seen so far */
+    int op;  /* bandera para el menú */
+
     char line[MAXLINE]; /* current input line */
     char longest[MAXLINE]; /* longest line saved here */
     max = 0;
+
+    printf("        MENÚ\n");
+    printf("1. Contar Caracteres\n");
+    printf("2. Contar Dígitos\n");
+    printf("3. Salir\n");
+    printf("Ingrese una opción: ");
+
+    scanf("%d",&op);
+    printf("\n");
+
+    switch(op)
+{
+    case 1:
 
         while ((len = getline2(line, MAXLINE)) > 0)
         if (len > max) {
             max = len;
             copy(longest, line);
         }
-         if (max=0) {
+
         printf("\nLa cadena mas larga es: %s\n", longest);
         printf("La cadena tiene %d caracteres\n", cuenta(longest)-1);
+        
+        break;
+
+    case 2:
+
+        while ((len = getline2(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+
+        printf("\nLa cadena mas larga es: %s\n", longest);
         printf("La cadena tiene %d dígito(s)\n", cuenta_digitos(longest));
-       }
-return 0;
+        
+        break;
+
+    case 3:
+       
+        exit(0);
+     
+        break;
+
+    default:
+
+        printf("Opción inválida\n");
+}
+    return 0;
 }
 
 /* getline2: read a line into s, return length */
